160a69d5d1849a1978d63f6a65ca348a40fcc1b9
Surendar <surendar.ofcl@gmail.com>
2026-02-28 11:06:15 +0530

    Added Prometheus monitoring support

--- app.js
+++ app.js
@@ -1,10 +1,45 @@
 const express = require('express');
-const app = express();
+const client = require('prom-client');
 
-app.get('/', (req, res) => {
-  res.send('DevOps Capstone Project - Surendar 🚀');
+const app = express();
+const port = 3000;
+
+// Create a Registry
+const register = new client.Registry();
+client.collectDefaultMetrics({ register });
+
+// Custom HTTP request counter
+const httpRequestCounter = new client.Counter({
+  name: 'http_requests_total',
+  help: 'Total number of HTTP requests',
+});
+register.registerMetric(httpRequestCounter);
+
+// Middleware to count requests
+app.use((req, res, next) => {
+  httpRequestCounter.inc();
+  next();
 });
 
-app.listen(3000, () => {
-  console.log('Application running on port 3000');
+// Routes
+app.get('/', (req, res) => {
+  res.send('DevOps Capstone App - Monitoring Enabled 🚀');
+});
+
+app.get('/about', (req, res) => {
+  res.send('About Page - CI/CD + Monitoring Project');
+});
+
+app.get('/health', (req, res) => {
+  res.json({ status: 'UP' });
+});
+
+// Metrics endpoint
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', register.contentType);
+  res.end(await register.metrics());
+});
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
 });
